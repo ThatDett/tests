9a8cc0dc2dc5aafde08b364d0a990a2b23b6e1ff
ThatDet <trorjk@gmail.com>
2025-05-28 02:01:49 -0300

    .

--- docs/pkg-data.js
+++ docs/pkg-data.js
@@ -1,10 +1,10 @@
-/** Generated with odin version dev-2025-04 (vendor "odin") Windows_amd64 @ 2025-05-28 04:53:25.323538300 +0000 UTC */
+/** Generated with odin version dev-2025-04 (vendor "odin") Windows_amd64 @ 2025-05-28 05:01:47.143385900 +0000 UTC */
 var odin_pkg_data = {
 "packages": {
 	"game": {
 		"name": "game",
 		"collection": "game",
-		"path": "/ThatDett/tests/docs/game/",
+		"path": "/game/",
 		"entities": [
 			{"kind": "t", "name": "Entity"},
 			{"kind": "t", "name": "Player"},
