98c33e6b4ef03d631e7c5feb9d6f5e172136e613
ThatDet <trorjk@gmail.com>
2025-05-28 01:16:20 -0300

    .

--- docs/pkg-data.js
+++ docs/pkg-data.js
@@ -1,4 +1,14 @@
-/** Generated with odin version dev-2025-04 (vendor "odin") Windows_amd64 @ 2025-05-28 03:51:35.671712800 +0000 UTC */
+/** Generated with odin version dev-2025-04 (vendor "odin") Windows_amd64 @ 2025-05-28 04:16:01.534983700 +0000 UTC */
 var odin_pkg_data = {
 "packages": {
-}};
+	"game": {
+		"name": "game",
+		"collection": "game",
+		"path": "/game/",
+		"entities": [
+			{"kind": "t", "name": "Entity"},
+			{"kind": "t", "name": "Player"},
+			{"kind": "t", "name": "Vector2"},
+			{"kind": "p", "name": "main"}
+		]
+	}}};
